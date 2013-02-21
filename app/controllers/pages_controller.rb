2c47046f5baad2a011caae1c5bc653bbac1ff0ed
tark000 <tark000@ukr.net>
2013-02-21 15:18:16 +0200

    date in page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < InheritedResources::Base
   def index
-    @pages = Page.all
+    @pages = Page.where("start_at < ? ", Time.now).where("finish_at > ? ", Time.now).all
     @post_calendar = Post.where(post_type_id:1).last
     @post_break = Post.where(post_type_id:2).last
     @about = About.last
