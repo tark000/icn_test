53087d38552cd760b6ed72453537d876444ce9a0
tark000 <tark000@ukr.net>
2013-02-24 12:54:42 +0200

    sdfas

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,7 +15,8 @@ set :branch, "master"
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
 
-after "deploy"
+after "deploy", "deploy:cleanup" # keep only the last 5 releases
+
 namespace :deploy do
   %w[start stop restart].each do |command|
     desc "#{command} unicorn server"
