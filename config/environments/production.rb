f612bfea716410eb123016b3a350cab9e543f7e5
StasKoval <koval.s.y@gmail.com>
2013-02-25 13:04:48 +0200

    new assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ IcnSite::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
@@ -56,7 +56,7 @@ IcnSite::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += ['foundation.min.js', 'foundation.css','script.js','app.css','fonts.js','cufon-yui.js','jquery.ui.all.js','jquery.cycle2.min.js']
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
