6c760d4d02eb289839744542f3352cb85f1846d1
tark000 <tark000@ukr.net>
2013-05-21 18:17:32 +0300

    search by street

--- app/models/street.rb
+++ app/models/street.rb
@@ -1,5 +1,22 @@
 class Street < ActiveRecord::Base
-	establish_connection :icn_houses
+
   attr_accessible :name
 	has_many :builds
+
+  def search_build(search2)
+    if search2
+      Strwhere(:street_id => params[:search2])
+    else
+      scoped
+    end
+
+  end
+
+  def self.search(search)
+    if search
+      where('name LIKE ?', "%#{search}%")
+    else
+      scoped
+    end
+  end
 end
