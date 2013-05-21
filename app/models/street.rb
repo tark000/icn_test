45b46894aeaf4908f9ec48d1cc429cb112e45af7
tark000 <tark000@ukr.net>
2013-05-21 18:26:42 +0300

    search by street

--- app/models/street.rb
+++ app/models/street.rb
@@ -1,16 +1,12 @@
 class Street < ActiveRecord::Base
-
+  establish_connection :icn_houses
   attr_accessible :name
 	has_many :builds
 
-  def search_build(search2)
-    if search2
-      Strwhere(:street_id => params[:search2])
-    else
-      scoped
-    end
 
-  end
+
+
+
 
   def self.search(search)
     if search
