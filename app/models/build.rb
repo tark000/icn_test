a2b3754829a2cf131a64bd6851ff0523a41e0336
tark000 <tark000@ukr.net>
2013-05-21 21:07:32 +0300

    search wark

--- app/models/build.rb
+++ app/models/build.rb
@@ -1,5 +1,5 @@
 class Build < ActiveRecord::Base
-#	establish_connection :icn_houses
+	establish_connection :icn_houses
   attr_accessible :name, :staus, :active, :street_id
 	belongs_to :street
 
@@ -11,12 +11,5 @@ class Build < ActiveRecord::Base
     end
   end
 
-  def self.s()
-    if search
-      where('name LIKE ?', "%#{2}%")
-    else
-      scoped
-    end
-  end
-
 end
+                     gi
