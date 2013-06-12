8b8536de1eb394975a1a57d6b24023f73113d024
tark000 <tark000@ukr.net>
2013-06-12 19:21:43 +0300

    cap

--- app/models/jurnal.rb
+++ app/models/jurnal.rb
@@ -1,5 +1,5 @@
 class Jurnal < ActiveRecord::Base
- # establish_connection :icn_houses
+  establish_connection :icn_houses
   set_table_name "jurnal"
   attr_accessible :date, :doc_name, :arxiv, :comment, :doc_type, :operator
 
