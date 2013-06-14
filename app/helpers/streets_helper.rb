6657a1621d93618e01c664137b9fdaf25ad5114a
tark000 <tark000@ukr.net>
2013-06-14 15:57:41 +0300

    street add all

--- app/helpers/streets_helper.rb
+++ app/helpers/streets_helper.rb
@@ -1,16 +1,13 @@
 module StreetsHelper
   def street
     @str = []
-    @str1 = []
-    @str2 = []
     Street.order(:name).all.each do |w|
       if w.builds.present?
-        @str1 << w
-      else
-        @str2 << w
+        @str << w
       end
     end
-    @str = @str1 + @str2
     return @str
   end
 end
+
+
