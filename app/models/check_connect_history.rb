de7f0a9122f0e656e6dca9e8bfe70cd2a498c136
tark000 <tark000@ukr.net>
2013-06-14 15:48:57 +0300

    street add all

--- app/models/check_connect_history.rb
+++ app/models/check_connect_history.rb
@@ -1,5 +1,5 @@
 class CheckConnectHistory < ActiveRecord::Base
-  #establish_connection :icn_houses
+  establish_connection :icn_houses
   set_table_name "check_connect_history"
   attr_accessible :id_doc, :check_connect_id, :streetid, :house, :apartment, :status, :date
 
