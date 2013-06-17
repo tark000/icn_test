b95ecaa028c82b111c3389a96ee30159f5ba6b14
tark000 <tark000@ukr.net>
2013-06-17 11:51:06 +0300

    cache

--- app/helpers/contact_helper.rb
+++ app/helpers/contact_helper.rb
@@ -1,2 +1,16 @@
 module ContactHelper
+  def street
+    @str = []
+    @str1 = []
+    @str2 = []
+    Street.order(:name).all.each do |w|
+      if w.builds.present?
+        @str1 << w
+      else
+        @str2 << w
+      end
+    end
+    @str = @str1 + @str2
+    return @str
+  end
 end
