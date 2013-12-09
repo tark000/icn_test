198d7f60f574096a1099403e5b1a119c7f7862f4
tark000 <tark000@ukr.net>
2013-12-09 16:08:42 +0200

    l

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,6 +9,7 @@ set :deploy_via, :remote_cache
 set :use_sudo, false
 
 set :scm, "git"
+#set :repository, "git@github.com:tark000/#{application}.git"
 set :repository, "git@bitbucket.org:grabetst/icn.git"
 set :branch, "master"
 
