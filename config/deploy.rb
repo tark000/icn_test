53c6f5a437947ba1b439745533a7f1f84d2606cb
tark000 <tark000@ukr.net>
2013-12-09 15:53:56 +0200

    l

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :deploy_via, :remote_cache
 set :use_sudo, false
 
 set :scm, "git"
-set :repository, "git@github.com:tark000/#{application}.git"
+set :repository, "git@bitbucket.org:grabetst/#{application}.git"
 set :branch, "master"
 
 default_run_options[:pty] = true
