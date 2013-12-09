6eefa32ca8dd96ca7a3cc6ee7870bd038274dbab
tark000 <tark000@ukr.net>
2013-12-09 15:56:01 +0200

    l

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :deploy_via, :remote_cache
 set :use_sudo, false
 
 set :scm, "git"
-set :repository, "git@bitbucket.org:grabetst/#{application}.git"
+set :repository, "git@bitbucket.org:grabetst/icn.git"
 set :branch, "master"
 
 default_run_options[:pty] = true
