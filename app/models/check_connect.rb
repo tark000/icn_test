99ba3818c502f70901160aa477630dc3217cac4c
tark000 <tark000@ukr.net>
2014-01-20 15:25:07 +0200

    change

--- app/models/check_connect.rb
+++ app/models/check_connect.rb
@@ -1,5 +1,5 @@
 class CheckConnect < ActiveRecord::Base
-  #establish_connection :icn_houses
+  establish_connection :icn_houses
   set_table_name "check_connect"
   attr_accessible :fio, :id_street, :house, :apartment, :email, :phone, :message, :add_time
 
