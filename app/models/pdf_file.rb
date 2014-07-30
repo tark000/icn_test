ce0ab922cc1f06c211ca39bdf993200b226c5fe4
taras <tark000@ukr.net>
2014-07-30 12:02:10 +0300

    deploy

--- app/models/pdf_file.rb
+++ app/models/pdf_file.rb
@@ -1,8 +1,10 @@
 class PdfFile < ActiveRecord::Base
   attr_accessible :file, :siza, :title, :translations_attributes
-  mount_uploader :file, FileUploader
+
 
 
   translates :file, :siza, :title
   accepts_nested_attributes_for :translations
+
+  Translation.mount_uploader :file, FileUploader
 end
